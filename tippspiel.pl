decb7473698f391c17da25540c4e18fc6d435c0b
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2010-05-27 22:48:07 +0200

    use dochere for header

--- tippspiel.pl
+++ tippspiel.pl
@@ -132,14 +132,16 @@ sub PrintDocument {
 # Return: HTML-String
 #	<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\"> \n \
 sub PrintHTMLHeader {
-    return
-        "Content-type: text/html\n\n \
-	<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"> \n \
-	<html xmlns=\"http://www.w3.org/1999/xhtml\"> \
-        <head><title>$GameName</title> \
-        <link rel=\"stylesheet\" type=\"text/css\" href=\"http://www.minaga-church.de/tippspiel/tipgame.css\" /> \
-	<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" /> \
-        </head>\n";
+    return <<EOT;
+Content-type: text/html
+
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
+<html xmlns=\"http://www.w3.org/1999/xhtml\">
+<head><title>$GameName</title>
+<link rel=\"stylesheet\" type=\"text/css\" href=\"http://www.minaga-church.de/tippspiel/tipgame.css\" />
+<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />
+</head>
+EOT
 }
 
 sub PrintHTMLTop {
